f9ac7dc0a61000b21e2c4681e43fbc35ac572f86
zqs78 <91004501+zqs78@users.noreply.github.com>
2026-08-29 13:40:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || 'd1cf4b9c-3e57-085d-b34a-797fcf601381';
+const UUID = process.env.UUID || '36963da5-b4dc-46bc-ab9e-dbecdc13d379';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      
-const SUB_PATH = process.env.SUB_PATH || 'vercel';           
-const NAME = process.env.NAME || 'Vercel';                       
+const SUB_PATH = process.env.SUB_PATH || 'cat';           
+const NAME = process.env.NAME || 'streamlit';                       
 const PORT = process.env.PORT || 3000;                    
 
 // NZ-Agent
